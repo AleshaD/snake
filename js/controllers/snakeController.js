b085ad95cdc2975dc367e6035cf56510e7e11a7a
Alesha <shadrov.al@gmail.com>
2016-09-08 18:24:07 +0300

    Сделано стыковка со стенами, поедание еды, рост змейки

--- js/controllers/snakeController.js
+++ js/controllers/snakeController.js
@@ -1,9 +1,10 @@
-snakeApp.controller('snakeController', ['$scope', '$interval', 'snakeService',
-    function ($scope, $interval, snakeService) {
+snakeApp.controller('snakeController', ['$rootScope','$scope', '$interval', 'snakeService',
+    function ($rootScope, $scope, $interval, snakeService) {
 
         $scope.snake = snakeService.snake;
 
         var play;
+        $scope.points = 0;
         $scope.play = function () {
 
             if ($scope.snake.parts.length == 0) $scope.snake._startCreate();
@@ -12,15 +13,31 @@ snakeApp.controller('snakeController', ['$scope', '$interval', 'snakeService',
 
             play = $interval(function () {
                 $scope.snake.move();
+                if ($scope.snake.wallConnect()) {
+                    $scope.stop();
+                }
+                if ($scope.snake.eatenFood()) {
+                    $scope.snake.growing();
+                    $scope.snake.foodCreate();
+                    $scope.points +=1;
+                }
             }, 300);
+
+            $scope.snake.foodCreate();
         };
 
         $scope.stop = function () {
             if ( angular.isDefined(play)) {
                 $interval.cancel(play);
                 play = undefined;
-                $scope.snake.parts.length = 0;
             }
+        };
+
+        $scope.reset = function () {
+            $scope.stop();
+            $scope.snake.parts.length = 0;
+            $scope.snake.direct = 100;
+            $scope.snake.foodDelete();
         };
 
         $scope.arrowDown = function (clickEvent) {
--- js/services/snakeService.js
+++ js/services/snakeService.js
@@ -8,11 +8,15 @@ snakeApp.factory ('snakeService', function (){
             parts: [],
 
             _startCreate: function () {
-                this.parts.push({id: 0, x: 80, y: 80});
+                this.parts.push({id: 0, x: 81, y: 153});
                 for (var i=1; i<4; i++){
-                    this.parts.push({id: i, x: this.parts[i-1].x+15, y: this.parts[i-1].y});
+                    this.parts.push({id: i, x: this.parts[i-1].x-9, y: this.parts[i-1].y});
                 }
                 },
+
+            aLive: function () {
+                return !!this.parts.length;
+            },
 
             move: function () {
                 var tempX = [], tempY = [];
@@ -22,13 +26,13 @@ snakeApp.factory ('snakeService', function (){
 
                     if (i == 0) {
                         switch (this.direct){
-                            case 119: this.parts[i].y -=10; // вверх
+                            case 119: this.parts[i].y -=9; // вниз
                                 break;
-                            case 100: this.parts[i].x +=10; // право
+                            case 100: this.parts[i].x +=9; // право
                                 break;
-                            case 97: this.parts[i].x -=10; // лево
+                            case 97: this.parts[i].x -=9; // лево
                                 break;
-                            case 115: this.parts[i].y +=10; //вниз
+                            case 115: this.parts[i].y +=9; //вверх
                                 break;
                             default : break;
                         }
@@ -45,21 +49,90 @@ snakeApp.factory ('snakeService', function (){
                 }
             },
 
+            direct: 100,
             whereGoing: function (keyPress) {
                 switch (keyPress) {
-                    case 119: this.direct = 119; //вверх
+                    case 119: this.direct = 119; //вниз
                         break;
                     case 100: this.direct = 100; // право
                         break;
                     case 97: this.direct = 97; // лево
                         break;
-                    case 115: this.direct = 115; //вниз
+                    case 115: this.direct = 115; //вверх
                         break;
                     default : break;
                 }
             },
 
-            direct: 100
+            foodParams: [],
+            foodCreate: function () {
+                var X, Y;
+
+                while (true) {
+                    X = this.randomValue(0,216);
+                    if (X % 9 == 0) {
+                        while (true) {
+                            Y = this.randomValue(0,216);
+                            if (Y % 9 == 0) {
+                                break;
+                            }
+                        }
+                        if (!this.checkConnection(X,Y)) break;
+                    }
+                }
+
+                if (this.aLive()) {
+                    this.foodParams[0] = { color: 'red', x: X,
+                        y: Y};
+                }
+            },
+
+            foodDelete: function () {
+                this.foodParams[0].color = 'white';
+            },
+
+            eatenFood: function () {
+                return this.checkConnection (this.foodParams[0].x, this.foodParams[0].y);
+            },
+
+            growing: function () {
+                this.parts.push({id: this.parts.length, x: this.foodParams[0].x, y: this.foodParams[0].y});
+            },
+
+            checkConnection: function (x, y) {
+                for (var i=0; i<this.parts.length; i++) {
+                    if ((x == this.parts[i].x) && (y == this.parts[i].y)) {
+                        return true;
+                    }
+                }
+                return false;
+            },
+
+            wallConnect: function () {
+                return (this.parts[0].x < 0 ||
+                        this.parts[0].x > 216 ||
+                        this.parts[0].y < 0 ||
+                        this.parts[0].y > 216)
+            },
+
+            randomValue: function (min, max) {
+                var rand = min - 0.5 + Math.random() * (max - min + 1);
+                rand = Math.round(rand);
+                return rand;
+            }
+
+            /*food: {
+                create: function () {
+                    //if (this.aLive()) {
+                        snake.food.params[0] = { color: '#ff0000', x: 100, //Math.random(0,225),
+                            y: 100}; // Math.random(0,225)
+                    //}
+                },
+                del: function () {
+                    this.food.params[0].color = '#ffffff';
+                },
+                params: []
+            }*/
 
             }
         }
